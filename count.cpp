692e3c07a0f835baae2ee5206dd6b7bb98168baa
Nguyễn Trang <trangnguyen@MacBook-Air-cua-Nguyen.local>
2023-07-24 21:53:39 +0930

    New

--- count.cpp
+++ count.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 
-// The `extern` keyword tells the compiler that the count function exists,
-// but will be implemented somewhere else
-extern int count(int[], int n);
-
-int main() {
-    int array[5] = {4,5,6,7,8};
-    std::cout << "The number is: " << count(array, 5) << std::endl;
-    return 0;
+int count(int array[], int n)
+{
+    int total = 0; 
+    for (int i = 0; i < n; i++) {
+        if (array[i] % 2 == 0) {
+            total += 1;
+        }
+    }
+    return total; 
 }
