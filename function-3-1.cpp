fa6411eb2f8a9b058006bc4b470da2df24c955b1
Nguyễn Trang <trangnguyen@MacBook-Air-cua-Nguyen.local>
2023-07-28 21:15:52 +0930

    jjj

--- function-3-1.cpp
+++ function-3-1.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 
 bool is_fanarray(int array[], int n) {
-  if (n <= 1) {
+  if (n < 1) {
     return false;
   } 
-  for (int i = 0; i < (n+1)/2; i++)
+  
+  for (int i = 0; i < n/2; i++)
   {
     if (array[i+1] < array[i])
     {
