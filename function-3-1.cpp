089205868b46addb54e73644fff9473cee66ce78
Edgyin42 <120734074+Edgyin42@users.noreply.github.com>
2023-07-29 18:12:24 +0930

    Update function-3-1.cpp

--- function-3-1.cpp
+++ function-3-1.cpp
@@ -3,22 +3,18 @@
 bool is_fanarray(int array[], int n) {
   if (n < 1) {
     return false;
-  } 
-  if ( array[0] != array[n])
-  {
-    for (int i = 1; i < (n+1)/2; i++)
-    {
-      if (array[i-1] > array[i])
-      {
-        if (array[i] != array[n-i])
-        {
-         return false; 
-        }
-        return false; 
-      }
-      return false; 
-    }
-    return false; 
   }
-  return true; 
+  if (array[0] != array[n]) {
+    return false;
+  }
+  for (int i = 1; i < (n + 1) / 2; i++) {
+    if (array[i - 1] > array[i]) {
+      if (array[i] != array[n - i]) {
+        return false;
+      }
+      return false;
+    }
+    return false;
+  }
+  return true;
 }
