85a4eb7edd6afe1e06214f8e8922219907084bc4
Edgyin42 <120734074+Edgyin42@users.noreply.github.com>
2023-07-29 16:47:54 +0930

    Delete main-3-2.cpp

--- main-3-2.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-extern int median_array(int array[], int n); 
-using namespace std; 
-int main()
-{
-    int a[] = {7,4,5,1,2,3,5,9,8};
-    int x = median_array(a, 9); 
-    cout <<"The median of the list is: "<< x << endl; 
-}
