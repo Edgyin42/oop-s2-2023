4126e192dd3ea8fdd0eacd500028b0cb3b15d300
Edgyin42 <120734074+Edgyin42@users.noreply.github.com>
2023-07-29 18:45:31 +0930

    Update function-3-1.cpp

--- function-3-1.cpp
+++ function-3-1.cpp
@@ -1,23 +1,44 @@
+// #include <iostream>
+
+// bool is_fanarray(int array[], int n) {
+//   if (n < 1) {
+//     return false;
+//   }
+//   if (array[0] != array[n]) {
+//     return false;
+//   }
+//   for (int i = 1; i < (n-1) / 2; i++) {
+//     if (array[i - 1] > array[i]) {
+//       return false;
+//     }
+//     if (i = (n-1)/2)
+//     {
+//       break;
+//     }
+//     if (array[i] != array[n - i]) {
+//         return false;
+//       }
+//   }
+//   return true;
+// }
+
 #include <iostream>
 
-bool is_fanarray(int array[], int n) {
-  if (n < 1) {
-    return false;
-  }
-  if (array[0] != array[n]) {
-    return false;
-  }
-  for (int i = 1; i < (n-1) / 2; i++) {
-    if (array[i - 1] > array[i]) {
-      return false;
-    }
-    if (i = (n-1)/2)
-    {
-      break;
-    }
-    if (array[i] != array[n - i]) {
+bool is_fanarray(int array[], int n){
+    if(n<1){
         return false;
-      }
-  }
-  return true;
+    }
+
+    for(int i=0;i<(n/2);i++){
+        if(array[i]>array[n/2]){
+            return false;
+        }
+    }
+    
+    for(int i=0;i<(n/2);i++){
+        if(array[i]!=array[n-1-i]){
+            return false;
+        }
+    }
+    return true;
 }
