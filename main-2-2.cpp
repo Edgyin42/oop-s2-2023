ff0ece0c559906514f2879729959f1807f818caf
Edgyin42 <120734074+Edgyin42@users.noreply.github.com>
2023-07-29 16:47:12 +0930

    Delete main-2-2.cpp

--- main-2-2.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream> 
-extern int max_element(int array[], int n);
-using namespace std; 
-int main(void)
-{
-    int n[] = {9,4,2,2,1,3,0};
-    int a = max_element(n, 7);
-    cout << "The max is " << a << endl; 
-}
