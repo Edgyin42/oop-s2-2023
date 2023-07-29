69b115744c3458af4d7621cbb8a6d304b4475a27
Edgyin42 <120734074+Edgyin42@users.noreply.github.com>
2023-07-29 16:47:45 +0930

    Delete main-2-4.cpp

--- main-2-4.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream> 
-extern bool is_ascending(int array[], int n);
-using namespace std; 
-int main(void)
-{
-    int a[] = {1,2,3,3,4,5,6,6,6,7};
-    bool x = is_ascending(a, 10);
-    cout << x << endl; 
-}
