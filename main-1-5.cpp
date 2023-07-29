f053de37684720fbd6d7e4a62a4e85f42b469b1c
Edgyin42 <120734074+Edgyin42@users.noreply.github.com>
2023-07-29 16:46:44 +0930

    Delete main-1-5.cpp

--- main-1-5.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std; 
-extern int count_evens(int number); 
-int main()
-{
-    int a = 12;
-    int count = count_evens(a); 
-    cout << "The number of even number is: "<< count << endl; 
-}
