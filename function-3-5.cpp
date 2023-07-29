2009b183a314d1e4247edeaf218dfaeee510bbe3
Edgyin42 <120734074+Edgyin42@users.noreply.github.com>
2023-07-29 17:14:30 +0930

    Update function-3-5.cpp

--- function-3-5.cpp
+++ function-3-5.cpp
@@ -9,6 +9,10 @@ double sum_even(double array[], int n)
     for (int i = 2; i < n; i+=2)
     {
         sum += array[i];
+        if (i == n-1)
+        {
+            break;
+        }
     }
     return sum; 
 }
