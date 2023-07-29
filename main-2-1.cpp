f8ef08151586ff3bb8abb4f6585a9c5629009755
Edgyin42 <120734074+Edgyin42@users.noreply.github.com>
2023-07-29 16:47:02 +0930

    Delete main-2-1.cpp

--- main-2-1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream> 
-extern int min_element(int array[], int n);
-using namespace std; 
-int main(void)
-{
-    int n[] = {9,4,2,2,1,3,0};
-    int a = min_element(n, 7);
-    cout << "The min is " << a << endl; 
-}
