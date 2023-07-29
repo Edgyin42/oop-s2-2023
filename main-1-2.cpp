674a4aabe00bdf486ed2cd65277d4c1d7aefd3df
Edgyin42 <120734074+Edgyin42@users.noreply.github.com>
2023-07-29 16:46:26 +0930

    Delete main-1-2.cpp

--- main-1-2.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream> 
-extern double array_mean(int array[], int n);
-using namespace std; 
-int main(void)
-{
-    int n[] = {1,2,3,4,5};
-    double a = array_mean(n, 5);
-    cout << "The mean is " << a << endl; 
-}
