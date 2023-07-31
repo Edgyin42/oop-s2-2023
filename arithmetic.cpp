130d64533575cfe126b18216878877fbee1d1ab7
Edgyin42 <120734074+Edgyin42@users.noreply.github.com>
2023-07-31 15:20:38 +0930

    Update arithmetic.cpp

--- arithmetic.cpp
+++ arithmetic.cpp
@@ -3,25 +3,25 @@
 using namespace std; 
 
 
-string AddBinary(string a, string b)
-{
-    string res; 
-    int sum, carry = 0;
-    int i = a.size() - 1; int j = b.size() - 1; 
-    while ( i>=0 || j>= 0 )
-    {
-        sum = carry; 
-        if (i>=0) sum+= a[i] - '0';
-        if (j>=0) sum+= b[j] - '0';
-        if (sum%2 == 0) res += '0'; 
-        else res += '1'; 
-        carry = sum/2; 
-        i--; j--;
-    }
-    if (carry == 1) res += '1'; 
-    reverse(res.begin(), res.end()); 
-    return res; 
-}
+// string AddBinary(string a, string b)
+// {
+//     string res; 
+//     int sum, carry = 0;
+//     int i = a.size() - 1; int j = b.size() - 1; 
+//     while ( i>=0 || j>= 0 )
+//     {
+//         sum = carry; 
+//         if (i>=0) sum+= a[i] - '0';
+//         if (j>=0) sum+= b[j] - '0';
+//         if (sum%2 == 0) res += '0'; 
+//         else res += '1'; 
+//         carry = sum/2; 
+//         i--; j--;
+//     }
+//     if (carry == 1) res += '1'; 
+//     reverse(res.begin(), res.end()); 
+//     return res; 
+// }
 
 // string SubtractBinary(string a, string b)
 // {
