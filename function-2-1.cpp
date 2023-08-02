031bcc8cdd7c61a99a2529bb54a2df9953905ac2
Edgyin42 <120734074+Edgyin42@users.noreply.github.com>
2023-08-02 14:46:08 +0930

    Update function-2-1.cpp

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -7,12 +7,8 @@ void print_binary_str(std::string decimal_number)
     string a = ""; 
     while (num > 0) 
     {
-        a += char((num%2)+'0');
+        a = char((num%2)+'0') + a;
         num/=2; 
     }
-    int b = a.length() - 1; 
-    for (int i = b; i > 0; i--)
-    {
-        cout << a[i]; 
-    }
+    cout << a; 
 }
