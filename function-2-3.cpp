4bea673093700df1220c74d26756c33a065e8dee
Edgyin42 <120734074+Edgyin42@users.noreply.github.com>
2023-08-03 15:59:44 +0930

    Update function-2-3.cpp

--- function-2-3.cpp
+++ function-2-3.cpp
@@ -6,23 +6,23 @@ bool is_palindrome(int integers[], int length)
 {
     if (length < 1)
     {
-        return 0; 
+        return false; 
     } 
     for (int i = 0; i < (length/2); i++)
     {
         if (integers[i] != integers[length-i-1])
         {
-            return 0; 
+            return false; 
         }
     }
     for (int i = 0; i < ((length-1)/2); i++)
     {
         if (integers[i+1] < integers[i])
         {
-            return 0; 
+            return false; 
         }
     }
-    return 1; 
+    return true; 
 }
 
 int sum_array_elements(int integers[], int length)
