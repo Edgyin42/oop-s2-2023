f0cbdcb4d2e4255dfde399af7c3ac9d14c9ee06d
Nguyễn Trang <trangnguyen@student-10-201-01-140.wireless.adelaide.edu.au>
2023-08-14 17:54:25 +0930

    a

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -16,52 +16,52 @@ void hexDigits(int *numbers, int length)
         switch(numbers[i])
         {
             case 0:
-                cout << i << " " << numbers[i] << numbers[i] << endl; 
+                cout << i << " " << numbers[i] << " " << numbers[i] << endl; 
             break; 
             case 1:
-                 cout << i << " " << numbers[i] << numbers[i] << endl; 
+                 cout << i << " " << numbers[i] << " " << numbers[i] << endl; 
             break; 
             case 2:
-                cout << i << " " << numbers[i] << numbers[i] <<endl; 
+                cout << i << " " << numbers[i] << " " << numbers[i] <<endl; 
             break; 
             case 3:
-                 cout << i << " " << numbers[i] << numbers[i] <<endl; 
+                 cout << i << " " << numbers[i] << " " << numbers[i] <<endl; 
             break; 
             case 4:
-                 cout << i << " " << numbers[i] << numbers[i] << endl; 
+                 cout << i << " " << numbers[i] << " " << numbers[i] << endl; 
             break; 
             case 5:
-                cout << i << " " << numbers[i] << numbers[i] << endl; 
+                cout << i << " " << numbers[i] << " " << numbers[i] << endl; 
             break; 
             case 6:
-                 cout << i << " " << numbers[i] << numbers[i] << endl; 
+                 cout << i << " " << numbers[i] <<" " <<  numbers[i] << endl; 
             break; 
             case 7:
-                 cout << i << " " << numbers[i] << numbers[i] << endl; 
+                 cout << i << " " << numbers[i] << " " << numbers[i] << endl; 
             break; 
             case 8:
-                 cout << i << " " << numbers[i] << numbers[i] << endl; 
+                 cout << i << " " << numbers[i] << " " << numbers[i] << endl; 
             break; 
             case 9:
-                 cout << i << " " << numbers[i] << numbers[i] << endl; 
+                 cout << i << " " << numbers[i] << " " << numbers[i] << endl; 
             break; 
             case 10:
-                 cout << i << " " << numbers[i] << "A" << endl; 
+                 cout << i << " " << numbers[i] << " " << "A" << endl; 
             break; 
             case 11:
-                 cout << i << " " << numbers[i] << "B" << endl; 
+                 cout << i << " " << numbers[i] << " " << "B" << endl; 
             break; 
             case 12:
-                 cout << i << " " << numbers[i] << "B" << endl; 
+                 cout << i << " " << numbers[i] << " " << "B" << endl; 
             break; 
             case 13:
-                 cout << i << " " << numbers[i] << "C" << endl; 
+                 cout << i << " " << numbers[i] << " " << "C" << endl; 
             break; 
             case 14:
-                 cout << i << " " << numbers[i] << "D" << endl; 
+                 cout << i << " " << numbers[i] << " " << "D" << endl; 
             break; 
             case 15:
-                 cout << i << " " << numbers[i] << "E" << endl; 
+                 cout << i << " " << numbers[i] << " " << "E" << endl; 
             break; 
         }
     }
