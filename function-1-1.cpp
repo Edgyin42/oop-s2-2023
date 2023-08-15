b29fb77718db4a44fc0ae78f09f424b35a2018be
Nguyễn Trang <trangnguyen@student-10-201-01-140.wireless.adelaide.edu.au>
2023-08-15 20:17:34 +0930

    a

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,20 +1,12 @@
 #include <iostream> 
-using namespace std; 
-int *readNumbers()
-{
-    int *arr = new int[10]; 
-    for (int i = 0; i < 10; i++)
-    {
-        cin >> arr[i]; 
-    }
-    return arr; 
-}
-void printNumbers(int *numbers, int length)
-{
-    for (int i = 0; i < length; i++)
-    {
-        cout << i << " " << numbers[i] << endl; 
-    }
-}
+#include "Person.h"
+Person* createPersonArray(int n) {
+    Person* personArray = new Person[n]; // Allocate memory for n Person structs
 
+    for (int i = 0; i < n; ++i) {
+        personArray[i].name = "John Doe";
+        personArray[i].age = 0;
+    }
 
+    return personArray;
+}
--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,10 +1,13 @@
 #include <iostream> 
-using namespace std; 
-extern int *readNumbers();
-extern void printNumbers(int *numbers, int length); 
-int main()
-{
-    int *arr = readNumbers();  
-    printNumbers(arr, 10); 
-    delete(arr); 
+#include "Person.h"
+extern Person* createPersonArray(int n); 
+int main() {
+    // Example usage
+    int n = 5;
+    Person* people = createPersonArray(n);
+
+    delete[] people;
+    return 0;
 }
+
+
