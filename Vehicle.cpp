b7a0a3fb9496f1c0767fc599d8c89001df043a41
Nguyễn Trang <Tr>
2023-08-28 08:05:54 +0930

    a

--- Vehicle.cpp
+++ Vehicle.cpp
@@ -20,22 +20,3 @@ int Vehicle::getParkingDuration(){
     return timePass; 
 }
 
-Car:: Car(int ID): Vehicle(ID){};  
-int Car::getParkingDuration(){
-    int currTime = time(nullptr); 
-    double timePass = difftime(currTime, getTimeOfEntry()) * 0.9;
-    return timePass; 
-}
-int Bus::getParkingDuration(){
-    int currTime = time(nullptr); 
-    double timePass = difftime(currTime, getTimeOfEntry()) * 0.75;
-    return timePass; 
-}
-Bus:: Bus(int ID): Vehicle(ID){};  
-
-int Motorbike::getParkingDuration(){
-    int currTime = time(nullptr); 
-    double timePass = difftime(currTime, getTimeOfEntry()) * 0.85;
-    return timePass; 
-}
-Motorbike:: Motorbike(int ID): Vehicle(ID){};  
--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,6 +1,9 @@
 
 #include <iostream>
 #include "Vehicle.h"
+#include "Bus.h"
+#include "Car.h"
+#include "Motorbike.h"
 using namespace std;
 int main(){
     Car a(10); 
