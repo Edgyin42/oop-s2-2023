e79ebefe65ff106fbe323371cd0e4c9c4a3dcda2
Nguyễn Trang <Tr>
2023-10-24 23:38:03 +1030

    A

--- Explosion.h
+++ Explosion.h
@@ -4,7 +4,7 @@ class Explosion: public GameEntity, public Effect{
     public:
     Explosion(int x, int y): GameEntity(x, y, 'E'){}
     void apply(GameEntity& entity) override {
-        this->position = std::make_tuple(-1, -1);
-        this->type = 'X'; 
+        entity.setPos(-1, -1);
+        entity.setType('X');
     };
 };
